b5bcf241bce04cd90039fe99464041483aca21b7
Yoga Cats <esherrow77@gmail.com>
2021-04-30 18:51:04 -0400

    fix selection criteria

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,25 +5,36 @@ var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','
 var symbols = [' ', '!', '"', '#', '$', '%', '&',"'", '('];
 
 function generatePassword(pswdLength) { 
-  var useNumber = window.prompt("Would you like to use digits 0-9?");
-  var useLower = window.prompt("Would you like to use lowercase letters a - z?");
-  var useUpper = window.prompt("Would you like to use uppercase letters A - z?");
-  var useSpec = window.prompt("Would you like to use special characters?");
   
-  for (var i = 0; i < pswdLength; i++) {
+  var useNumber = window.confirm("Would you like to use digits 0-9?");
+  var useLower = window.confirm("Would you like to use lowercase letters a - z?");
+  var useUpper = window.confirm("Would you like to use uppercase letters A - Z?");
+  var useSpec = window.confirm("Would you like to use special characters?");
+  var i =0;
+  
+  while(i < pswdLength){
+  //for (var i = 0; i < pswdLength; i++) {
     var use = Math.floor(Math.random()*4);
     var letterOpt = Math.floor(Math.random()*letters.length);
     console.log(i);  
     if (use === 0 && useNumber == true){
-      psword.push(Math.floor(Math.random()*10));}
-    else if (use === 1){//} && useUpper == true){
-      psword.push(letters[letterOpt]);}
-    else if (use === 2){//} && useLower == true){
+      psword.push(Math.floor(Math.random()*10));
+      console.log("a")
+      i++;}
+    else if (use === 1 && useUpper == true){
+      psword.push(letters[letterOpt]);
+      console.log('b')
+      i++;}
+    else if (use === 2 && useLower == true){
       var lowerCase = letters[letterOpt].toLowerCase();
-      psword.push(lowerCase);}
-    else if (use === 3){//} && useSpec == true){
+      psword.push(lowerCase);
+      console.log('c')
+      i++;}
+    else if (use === 3 && useSpec == true){
       var special = Math.floor(Math.random()*symbols.length);
-      psword.push(symbols[special]);};
+      psword.push(symbols[special]);
+      console.log('d')
+      i++};
     //else{
       //i--;
     //};   
@@ -52,4 +63,5 @@ function writePassword() {
 };
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
